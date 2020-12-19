1106a2c5fbce134ca1aa93931bf554ba3853bda7
Cyrus Tabatabai-Yazdi <24766642+cyrustabatab@users.noreply.github.com>
2020-12-18 22:53:01 -0800

    added working contact me page that sends email whenever user tries to contact

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 import requests
+import smtplib
+import os
 
 print(__name__)
 app = Flask(__name__)
 
 url = 'https://api.npoint.io/43644ec4f0013682fc0d'
-
 posts = requests.get(url).json()
 
 for post in posts:
@@ -20,9 +21,23 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route('/contact')
+@app.route('/contact',methods=['GET','POST'])
 def contact():
-    return render_template('contact.html')
+    if request.method == 'GET':
+        return render_template('contact.html',message_sent=False)
+    else:
+        name = request.form['name']
+        email = request.form['email']
+        phone = request.form['phone']
+        message = request.form['message']
+        my_email = "calcguru2020@gmail.com"
+        to_address = "ctabatab@gmail.com"
+        with smtplib.SMTP("smtp.gmail.com",port=587) as connection:
+            connection.starttls()
+            connection.login(user=my_email,password=os.environ.get('PASSWORD'))
+            connection.sendmail(from_addr=my_email,to_addrs=to_address,msg=f"Subject: Contact!\n\nName: {name}\n Email: {email}\n Phone: {phone}\nMessage: {message}")
+        
+        return render_template('contact.html',message_sent=True)
 
 @app.route('/post/<int:post_id>')
 def get_post(post_id):
@@ -34,6 +49,12 @@ def get_post(post_id):
 
     return render_template('post.html',post=post)
 
+
+
+
+
+
+    
 if __name__ == "__main__":
     
     app.run(debug=True)
